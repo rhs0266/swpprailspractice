78e1c87337a896db2f736abdc78972ebe03e7bed
Ryu Ho Seok <dkflfkdeh@gmail.com>
2015-03-30 20:22:54 +0900

    Showing Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,22 @@
 class ArticlesController < ApplicationController
+    def show
+        @article = Article.find(params[:id])
+    end
     def new
     end
     def create
-        render plain: params[:article].inspect
+        @article = Article.new(article_params)
+        # Initialize new model with filtered parameter.
+        @article.save
+        # Store the model to DB
+        # Returns success or failure as a boolean value
+        redirect_to @article
+        # Redirect automatically to 'show' action, defined later.
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title, :text)
+        # Whitelist our controller parameters to prevent wrongful mass assignment
     end
 end
